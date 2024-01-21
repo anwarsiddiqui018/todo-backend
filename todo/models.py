173c74471724c2c0917a81c0daf7130957f52b8e
anwarsiddiqui018 <siddiqui.anwar018@gmail.com>
2024-01-21 17:30:08 +0530

    Initial commit

---
+++ todo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  # Use the ID of a default user
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    #set to current time
+    created = models.DateTimeField(auto_now_add = True)
+    completed = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.title
+
+
